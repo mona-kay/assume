a4ac4950e5a65c3831fea6a28e04736451c136e0
Mona <42977162+mona-kay@users.noreply.github.com>
2019-03-09 22:42:20 -0500

    Update normal.R

--- normal.R
+++ normal.R
@@ -1,37 +1,32 @@
 # Test the normality of numeric columns
 normal <- function(df) {
   for (x in colnames(data.frame(df))) {
-    l <- length(df[[x]])
+    if(class(df[[x]]) == "numeric") {
+      l <- length(df[[x]])
+      m <- mean(df[[x]], na.rm = TRUE)
+      s1 <- sum((df[[x]]-m)^3, na.rm = TRUE)
+      s2 <- sum((df[[x]]-m)^2, na.rm = TRUE)
+      skew <- (s1/l)/(s2/l)^(3/2)
+      skew <- round(skew, digits = 2)
+    } else {}
     ifelse(class(df[[x]]) != "numeric", df[[x]],
-           ifelse((sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                    (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) >= 1,
+           ifelse(skew >= 1,
                   print(paste0(x," is highly positively skewed, with a skewness of ",
-                               (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                 (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2))),
-                  ifelse((sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                           (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) <= -1,
+                               skew)),
+                  ifelse(skew <= -1,
                          print(paste0(x," is highly negatively skewed, with a skewness of ",
-                                      (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                        (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2))),
-                         ifelse((sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                  (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) > -1 &
-                                  (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                  (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) <= -.5,
+                                      skew)),
+                         ifelse(skew > -1 & skew <= -.5,
                                 print(paste0(x," is moderately negatively skewed, with a skewness of ",
-                                             (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                               (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2))),
-                                ifelse((sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                         (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) < 1 &
-                                         (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                         (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2) >= .5,
+                                             skew)),
+                                ifelse(skew < 1 & skew >= .5,
                                        print(paste0(x," is moderately positively skewed, with a skewness of ",
-                                                    (sum((df[[x]]-mean(df[[x]]))^3)/l)/
-                                                      (sum((df[[x]]-mean(df[[x]]))^2)/l)^(3/2))),
+                                                    skew)),
                                        print(paste0(x, " is not skewed."))
-                                )
-                         )
-                  )
-           )
-    )
-  }
+                                  )
+                           )
+                    )
+             )
+      )
+    }
 }
